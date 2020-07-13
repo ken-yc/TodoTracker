fee5e6fbf6190fbc7ff215ce034d212dffd00660
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2020-07-13 16:13:29 -0500

    components

--- todo-list/src/app/app.module.ts
+++ todo-list/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { MenuComponent } from './menu/menu.component';
+import { RightComponent } from './right/right.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent,
+    MenuComponent,
+    RightComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
