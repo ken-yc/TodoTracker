db68f925b13ca07879f60dfc0eb7f33691ce3cf1
ken-yc <12385996+ken-yc@users.noreply.github.com>
2020-07-14 14:23:23 -0700

    Added edit, delete, and mark as complete

--- todo-list/src/app/edit/edit.component.ts
+++ todo-list/src/app/edit/edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { FormGroup, FormControl} from '@angular/forms';
+
+import { TodoService } from '../services/todo.service';
+import { Todo } from '../ITodo';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
+  id: number;
+  todo: Todo;
 
-  constructor() { }
+  editForm = new FormGroup({
+    title: new FormControl(),
+    id: new FormControl(),
+    completed: new FormControl()
+  });
+
+  constructor(private router: Router, private route: ActivatedRoute, private todoService: TodoService) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      const id = 'id';
+      this.id = params[id];
+    });
+
+    const observable = this.todoService.getTodo(this.id);
+    observable.subscribe(data => {
+      this.todo = data;
+      this.editForm.get('title').setValue(data.title);
+      this.editForm.get('id').setValue(data.id);
+      this.editForm.get('completed').setValue(data.completed);
+    });
+  }
+
+  editTodo(todoSub: FormGroup): void {
+    const form = JSON.stringify(todoSub.value);
+    this.todoService.editTodo(form).subscribe(
+      rsponse => {
+        //console.log("success");
+        this.router.navigate(['/todos']);
+      }
+    );
   }
 
 }
