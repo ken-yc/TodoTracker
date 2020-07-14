598184b7717ddd0229f4c88403f0ea8d7514cd18
ken-yc <12385996+ken-yc@users.noreply.github.com>
2020-07-14 12:13:22 -0700

    Added TodoList View, Todo View and Add/Edit Todo components

--- todo-list/src/app/app-routing.module.ts
+++ todo-list/src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './main/main.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
-
+import { ViewComponent } from './view/view.component';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
 
 const routes: Routes = [
   { path: 'main', component: MainComponent },
-  { path: 'viewTodos', component: TodoListComponent },
+  { path: 'todos', component: TodoListComponent },
+  { path: 'todos/add', component: AddComponent},
+  { path: 'todos/edit/:id', component: EditComponent},
+  { path: 'todos/:id', component: ViewComponent},
   { path: '**', redirectTo: 'main' }
 ];
 
